cf604125b0dd0794290fcdef25c2349a716f3b7e
Tiago Prestes <bptiago123@gmail.com>
2025-09-24 15:26:20 -0300

    Add DesignToken

---
+++ Nano2/Resources/DesignToken.swift
@@ -0,0 +1,15 @@
+//
+//  DesignToken.swift
+//  Nano2
+//
+//  Created by Tiago Prestes on 24/09/25.
+//
+
+import Foundation
+
+enum DesignToken {
+    static let largeTitle: CGFloat = 34
+    static let title1: CGFloat = 28
+    static let title2: CGFloat = 22
+    static let body: CGFloat = 17
+}
--- Nano2/Views/StartingView.swift
+++ Nano2/Views/StartingView.swift
@@ -12,6 +12,7 @@ class StartingView: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = .white
+        addSubviews()
     }
     
     required init?(coder: NSCoder) {
@@ -19,19 +20,69 @@ class StartingView: UIView {
     }
     
     
-    // MARK: UI
-    let appName: UILabel = {
+    /// Elementos
+    let appTitle: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.numberOfLines = 0
         label.text = "Como planejar um rolê"
-        label.font = .preferredFont(forTextStyle: .largeTitle)
+        label.font = .systemFont(ofSize: DesignToken.largeTitle, weight: .bold)
+        
         return label
     }()
     
-//    let stackView: UIStackView = {
-//    
-//    }()
+    let appSubtitle: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 0
+        label.text = "(de última hora)"
+        label.font = .systemFont(ofSize: DesignToken.body, weight: .semibold)
+        label.textColor = .gray
+        label.textAlignment = .center
+        
+        return label
+    }()
+    
+    /// Layout / Stacks
+    lazy var labelStackView: UIStackView = {
+        let stack = UIStackView()
+        
+        stack.addArrangedSubview(appTitle)
+        stack.addArrangedSubview(appSubtitle)
+        stack.axis = .vertical
+        stack.spacing = 4
+        
+        return stack
+    }()
+    
+    lazy var mainStackView: UIStackView = {
+        let stack = UIStackView(
+            arrangedSubviews: [labelStackView]
+        )
+        
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        
+        stack.axis = .vertical
+        stack.alignment = .center
+        stack.spacing = 32
+        
+        return stack
+    }()
+    
+    /// Setup
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            mainStackView.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            mainStackView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            mainStackView.leadingAnchor.constraint(greaterThanOrEqualTo: leadingAnchor, constant: 16),
+            mainStackView.trailingAnchor.constraint(lessThanOrEqualTo: trailingAnchor, constant: -16)
+        ])
+    }
+    
+    private func addSubviews() {
+        addSubview(mainStackView)
+        setupConstraints()
+    }
 }
 
 
